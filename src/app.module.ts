0b64e74a81818ce2db6635f7524dd45f197f58fc
Ilia Krylatykh <iliakrylatykh@gmail.com>
2024-02-08 15:15:33 +0400

    swagger main setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
+import { Module } from '@nestjs/common'
+import { AuthModule } from './auth/auth.module'
+import { PrismaService } from './prisma.service'
 
 @Module({
-  imports: [AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [AuthModule],
+	controllers: [],
+	providers: [PrismaService]
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,37 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 
 	app.setGlobalPrefix('api')
 	app.enableCors()
+
+	const config = new DocumentBuilder()
+		.setTitle('Krylshop')
+		.setDescription('api documentation')
+		.setVersion('1.0')
+		.addBearerAuth(
+			{
+				type: 'http',
+				scheme: 'bearer',
+				bearerFormat: 'JWT',
+				name: 'JWT',
+				description: 'Enter JWT token',
+				in: 'header'
+			},
+			'JWT-auth'
+		)
+		.build()
+	const document = SwaggerModule.createDocument(app, config)
+	const options = {
+		swaggerOptions: {
+			docExpansion: 'none'
+		}
+	}
+	SwaggerModule.setup('api', app, document, options)
+
 	await app.listen(4000)
 }
 bootstrap()
